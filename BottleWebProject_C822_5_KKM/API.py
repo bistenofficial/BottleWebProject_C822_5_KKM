fd3301d3dd080aaf7a52fa872d6928fb9c3d5466
Sergey <ksenof.serrr@mail.ru>
2021-05-30 18:32:44 +0300

    form handler reworked

--- BottleWebProject_C822_5_KKM/API.py
+++ BottleWebProject_C822_5_KKM/API.py
@@ -1,23 +1,21 @@
-from bottle import post, request
+from bottle import  route, view, template, post, request
 import math
 import random
-@post('/three-chanel_system_with_rejection', method='post')
+from datetime import datetime
 
-def flowOfApplicationsGen():
-    timeEnd = int(request.forms.get('T1')) * 60
-    multiplier = request.forms.get('A')
+@post('/three-chanel_system_with_rejection', method='post')
+@route('/three-chanel_system_with_rejection')
+@view('three-chanel_system_with_rejection')
+def threeChanelSystemWithRejection():
+    timeEnd = float(request.forms.get('T2'))
+    multiplier = int(request.forms.get('A'))
     currentrequest = 0;
     requests = []
     while currentrequest <= timeEnd:
         requests.append(currentrequest)
-        nextRequest = (1 / multiplier) * math.log10(random.randint(0,10) / 10)
+        nextRequest = round((1 / multiplier) * math.log10(random.randint(1,10)),2)
         currentrequest = currentrequest + nextRequest
-    return requests
-
-@post('/three-chanel_system_with_rejection', method='post')
-
-def threeChanelSystemWithRejection(requests):
-    timeOnProcessing = request.forms.get('T2') 
+    timeOnProcessing = int(request.forms.get('T1')) 
     CurrentRequestinTheFirstChannel = 0
     CurrentRequestinTheSecondChannel = 0
     CurrentRequestinTheThirdChannel = 0
@@ -25,7 +23,7 @@ def threeChanelSystemWithRejection(requests):
     secondChanel = []
     thirdChanel = []
     serviced = []
-    for i in requset:
+    for i in requests:
         if (i >= CurrentRequestinTheFirstChannel):
             firstChanel.append(i + timeOnProcessing)
             CurrentRequestinTheFirstChannel = i + timeOnProcessing
@@ -46,5 +44,13 @@ def threeChanelSystemWithRejection(requests):
             serviced.append(1)
         else:
             serviced.append(0)
-        arr = [requests, firstChanel, secondChanel, thirdChanel,serviced]
-        return arr
+
+
+    arr = [requests, firstChanel, secondChanel, thirdChanel,serviced]
+
+    requests = [*arr[0]]
+    firstChanel = [*arr[1]]
+    secondChanel = [*arr[2]]
+    thirdChanel = [*arr[3]]
+    serviced = [*arr[4]]
+    return dict(output = arr, title='About', message='Your application description page.', year=datetime.now().year)
