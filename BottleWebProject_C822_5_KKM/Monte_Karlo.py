42f1ad855d8e4f2289f787384104b171dbdf778c
Maxim <58148359+bistenofficial@users.noreply.github.com>
2021-05-31 09:47:09 +0300

    Ввод и проверки сделаны

--- BottleWebProject_C822_5_KKM/Monte_Karlo.py
+++ BottleWebProject_C822_5_KKM/Monte_Karlo.py
@@ -1,20 +1,22 @@
 import random
-from bottle import post, request 
+from bottle import post, request
+import random
 
 @post("/home", method="post")
-def prepare_organiz():  #Метод для добавления нового партнера
+def Monte():  
     PA = request.forms.get('PA') #Считывание данных
     PB = request.forms.get('PB')
     PC = request.forms.get('PC')
     PD = request.forms.get('PD')
     PE = request.forms.get('PE')
+    rnd = round(random.random())
     if not PA: #Проверка на заполненность
-        return "Enter Duration of service of the request"
+        return "Enter P(A)"
     if not PB:
-        return "Enter Working hours"
+        return "Enter P(B)"
     if not PC:
-        return "Enter Alpha"
+        return "Enter P(C)"
     if not PD:
-        return "Enter Alpha"
+        return "Enter P(D)"
     if not PE:
-        return "Enter Alpha"
+        return "Enter P(E)"
