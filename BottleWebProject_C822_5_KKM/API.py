898165eeb905a21842dac4843a5f1d01a9b41bc7
Sergey <ksenof.serrr@mail.ru>
2021-06-01 13:37:22 +0300

    reworked output\ changed form handler

--- BottleWebProject_C822_5_KKM/API.py
+++ BottleWebProject_C822_5_KKM/API.py
@@ -1,11 +1,9 @@
-from bottle import  route, view, template, post, request
+from bottle import template, post, request
 import math
 import random
 from datetime import datetime
 
 @post('/three-chanel_system_with_rejection', method='post')
-@route('/three-chanel_system_with_rejection')
-@view('three-chanel_system_with_rejection')
 def threeChanelSystemWithRejection():
     timeEnd = float(request.forms.get('T2'))
     multiplier = int(request.forms.get('A'))
@@ -23,37 +21,39 @@ def threeChanelSystemWithRejection():
     secondChanel = []
     thirdChanel = []
     serviced = []
+    numOfServiced = 0
     for i in requests:
         if (i >= CurrentRequestinTheFirstChannel):
             firstChanel.append(round((i + timeOnProcessing),2))
             CurrentRequestinTheFirstChannel = i + timeOnProcessing
-            secondChanel.append(0)
-            thirdChanel.append(0)
+            secondChanel.append(' ')
+            thirdChanel.append(' ')
             serviced.append(1)
+            numOfServiced +=1
         elif (i >= CurrentRequestinTheSecondChannel):
             secondChanel.append(round((i + timeOnProcessing),2))
             CurrentRequestinTheSecondChannel = i + timeOnProcessing
-            thirdChanel.append(0)
-            firstChanel.append(0)
+            thirdChanel.append(' ')
+            firstChanel.append(' ')
             serviced.append(1)
+            numOfServiced +=1
         elif(i >= CurrentRequestinTheThirdChannel):
             thirdChanel.append(round((i + timeOnProcessing),2))
             CurrentRequestinTheThirdChannel = i + timeOnProcessing
-            firstChanel.append(0)
-            secondChanel.append(0)
+            firstChanel.append(' ')
+            secondChanel.append(' ')
             serviced.append(1)
+            numOfServiced +=1
         else:
-            firstChanel.append(0)
-            secondChanel.append(0)
-            thirdChanel.append(0)
+            firstChanel.append(' ')
+            secondChanel.append(' ')
+            thirdChanel.append(' ')
             serviced.append(0)
-
-
+    requests.append('Stop')
+    serviced.append("Number of serviced = " + (str)(numOfServiced))
+    firstChanel.append(' ')
+    secondChanel.append(' ')
+    thirdChanel.append(' ')
     arr = [requests, firstChanel, secondChanel, thirdChanel,serviced]
 
-    requests = [*arr[0]]
-    firstChanel = [*arr[1]]
-    secondChanel = [*arr[2]]
-    thirdChanel = [*arr[3]]
-    serviced = [*arr[4]]
-    return dict(output = arr, title='About', message='Your application description page.', year=datetime.now().year)
+    return template('three-chanel_system_with_rejection.html',output = arr, title='About', message='Your application description page.', year=datetime.now().year)
