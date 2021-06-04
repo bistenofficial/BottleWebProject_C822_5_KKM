01b3340c9deb47ad0a324b3c70160e8b6c592172
Maxim <58148359+bistenofficial@users.noreply.github.com>
2021-06-04 13:08:09 +0300

    Unit test added

--- BottleWebProject_C822_5_KKM/check.py
+++ BottleWebProject_C822_5_KKM/check.py
@@ -5,5 +5,8 @@ from datetime import datetime
 re_field = r"(^(1\.0))|(^[0]*[.][0-9]+$)|(^0)"
 
 def fieldTest(field):
-    result =  re.fullmatch(re_field, field) is not None
-    return result
+    result =  re.fullmatch(re_field, field)
+    if result:
+       return True
+    else:
+       return False
--- BottleWebProject_C822_5_KKM/test_InputCheck.py
+++ BottleWebProject_C822_5_KKM/test_InputCheck.py
@@ -1,8 +1,8 @@
 import unittest
 import form_handler
 
-list_input_false = ["asdasd", "001231", "-123", "+-=-=-=-","(123)","+12","+12.2","-14/3", ""]
-list_input_true = ["1", "20", "13.2","0.0001","1232"]
+list_input_false = ["fdsdfsegfdgb","0222","-0.22","           0.22","(1.2)","4.5"]
+list_input_true = ["1.0", "0.22", "0.0002","0.0001","0.3333221"]
 
 class Test_test_1(unittest.TestCase):
     def test_A(self):
@@ -14,3 +14,4 @@ class Test_test_1(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
